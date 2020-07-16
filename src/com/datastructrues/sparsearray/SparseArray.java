a20bddb771d5563a8eb51e004a257361e5adbe6b
sunbrought <577859767@qq.com>
2020-07-17 01:37:08 +0800

    数据结构，稀疏数组(写入磁盘)

--- src/com/datastructrues/sparsearray/SparseArray.java
+++ src/com/datastructrues/sparsearray/SparseArray.java
@@ -1,7 +1,11 @@
 package com.datastructrues.sparsearray;
+
+import java.io.*;
+import java.nio.Buffer;
+
 /*稀疏数组*/
 public class SparseArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*创建一个原始的二维数组 11*11
         * 0表示没有棋子，1表示黑子，2表示白子
         *  */
@@ -27,6 +31,7 @@ public class SparseArray {
         }
         System.out.println("sum:"+sum);
         //2.创建对应的稀疏数组
+
         int[][] sparseArr=new int[sum+1][3];
         //3.给稀疏数组赋值
         sparseArr[0][0]=11;
@@ -34,7 +39,7 @@ public class SparseArray {
         sparseArr[0][2]=sum;
         //4.遍历二维数组，将非0的值存放到sparseArr中
         int count=0;//创建一个计数器，记录是第几个非0的数据
-        for(int i=0;i<11;i++){
+        for(int i=0;i<11;i++){        
             for(int j=0;j<11;j++){
                 if(chessArr1[i][j]!=0){
                     count++;
@@ -44,6 +49,7 @@ public class SparseArray {
                 }
             }
         }
+        write(sparseArr);
         //输出稀疏数组
         System.out.println("得到的稀疏数组为：");
         for(int[] row : sparseArr){
@@ -65,5 +71,70 @@ public class SparseArray {
             }
             System.out.println();
         }
+        sparseArr=read();
+        System.out.println("读取磁盘的稀疏数组！");
+        for(int[] row : sparseArr){
+            for(int data : row){
+                System.out.print(data+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void write(int sparseArr[][]){
+        System.out.println("文件写入中----");
+        File file=new File("F:/sparseArray.txt");
+        BufferedWriter bufferedWriter;
+        try {
+            bufferedWriter=new BufferedWriter(new FileWriter(file));
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            StringBuffer allBuffer=new StringBuffer();
+            for(int[] rows : sparseArr){
+                StringBuffer rowsBuffer=new StringBuffer();
+                for(int data : rows){
+                    rowsBuffer.append(data+"\t");
+                }
+                allBuffer.append(rowsBuffer+"\n");
+            }
+            bufferedWriter.write(String.valueOf(allBuffer));
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static int[][] read() throws IOException {
+        System.out.println("读取磁盘的数据中~~~~~~");
+        File file=new File("F:/sparseArray.txt");
+        int[][] sparseArr = null;
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            String row = null;
+            try {
+                row=bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //分割成字符串返回数组类型
+            String[] s = new String(row).split("\t");
+            sparseArr = new int[Integer.parseInt(s[0])][Integer.parseInt(s[1])];
+            while ((row = bufferedReader.readLine()) != null) {
+                String[] s2 = new String(row).split("\t");
+                sparseArr[Integer.parseInt(s2[0])][Integer.parseInt(s2[1])] = Integer.parseInt(s2[2]);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return sparseArr;
     }
 }
